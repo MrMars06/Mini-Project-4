1983c3ed3c71d9ae8162d0c0f5d2788d2ca5969b
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-01 18:52:59 +0000

    Added test (don't know what to add yet)

--- Main.java
+++ Main.java
@@ -5,6 +5,10 @@ class Main {
     //new Scanner
     Scanner scan = new Scanner(System.in);
 
+    //Asking for Test (don't know what to add yet)
+    System.out.println("Test");
+    int age = scan.nextInt();
+
 
   }
 }
