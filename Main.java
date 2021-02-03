6e4bab441adb990df562eaeafee2a73c8ae02e8f
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-03 16:19:09 +0000

    Added method for adding both ages

--- Main.java
+++ Main.java
@@ -7,9 +7,21 @@ class Main {
 
     //Asking for age
     System.out.println("How old are you?");
-    int age = scan.nextInt();
+    int yourAge = scan.nextInt();
+    System.out.println("How old is your best friend?");
+    int theirAge = scan.nextInt();
+
+    int getAge = totalAge(yourAge, theirAge);
+
     
 
-
   }
+
+  //method for adding both ages
+  static int totalAge(int yourAge, int theirAge){
+    int getAge = yourAge + theirAge;
+    return getAge;
+  }
+
+  
 }
