5770d9b3038ad675bc466ecec30663b619f5920e
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-03 16:25:28 +0000

    Added some comments

--- Main.java
+++ Main.java
@@ -11,8 +11,10 @@ class Main {
     System.out.println("How old is your best friend?");
     int theirAge = scan.nextInt();
 
+    //creates int for the total age
     int getAge = totalAge(yourAge, theirAge);
 
+    //prints first method get age
     System.out.println("Both of your ages added together is " + getAge + ". Wow!");
 
   }
