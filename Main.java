e63de766f66dd7909d74bdae3ade55715fff3052
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-03 16:34:33 +0000

    created print statements about bones and created ints for bones.

--- Main.java
+++ Main.java
@@ -10,9 +10,15 @@ class Main {
     int yourAge = scan.nextInt();
     System.out.println("How old is your best friend?");
     int theirAge = scan.nextInt();
+    System.out.println("How many bones have you broken?");
+    int yourBones = scan.nextInt();
+    System.out.println("How many bones has your friends broken?");
+    int theirBones = scan.nextInt();
 
     //creates int for the total age
     int getAge = totalAge(yourAge, theirAge);
+    //creates int for the total bones
+    int getBones = totalBones(yourBones, theirBones);
 
     //prints first method get age
     System.out.println("Both of your ages added together is " + getAge + ". Wow!");
@@ -26,4 +32,5 @@ class Main {
   }
 
   
+
 }
