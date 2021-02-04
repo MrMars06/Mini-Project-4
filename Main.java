fe92949979812ed88290950916465f2770d645f6
Marcelo Domhoff <marcelo.domhoff@patelhs.org>
2021-02-04 22:31:05 +0000

    Added everything with related to bones back

--- Main.java
+++ Main.java
@@ -10,15 +10,20 @@ class Main {
     int yourAge = scan.nextInt();
     System.out.println("How old is your best friend?");
     int theirAge = scan.nextInt();
-    
+    System.out.println("How many bones have YOU broken?");
+    int yourBones = scan.nextInt();
+    System.out.println("How many bones has your FRIEND broken?");
+    int theirBones = scan.nextInt();
 
     //creates int for the total age
     int getAge = totalAge(yourAge, theirAge);
+    //creates int for total broken bones
+    int getBones = totalBones(yourBones, theirBones);
 
     //prints first method get age
     System.out.println("Both of your ages added together is " + getAge + ". Wow!");
     
-  
+    System.out.println("Both of you combined have broken up to " + getBones + " bones! Hope you don't break any more anytime soon!");
   }
 
   //method for adding both ages
@@ -27,6 +32,9 @@ class Main {
     return getAge;
   }
 
-  
+  static int totalBones(int yourBones, int theirBones){
+    int getBones = yourBones + theirBones;
+    return getBones;
+  }
   
 }
